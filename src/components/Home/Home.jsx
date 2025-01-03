a623af7bfa5d0698874bff84c8ddb450be408889
Swopnil Panday <swopnil100@gmail.com>
2025-01-03 16:17:51 -0500

    c

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { 
-  EnvelopeIcon,
   CheckCircleIcon 
 } from '@heroicons/react/24/outline';
 import AnimatedHero from '../Animated/AnimatedHero';
